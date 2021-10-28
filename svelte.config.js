ad95fa433260f0e88d83a19047fc46dcde8b7e95
ivensleos <ivensleos8@gmail.com>
2021-10-28 17:31:19 -0500

    Avance

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,6 @@
 /** @type {import('@sveltejs/kit').Config} */
 
+import netlify from '@sveltejs/adapter-netlify'
 import vercel from '@sveltejs/adapter-vercel'
 import node from '@sveltejs/adapter-node'
 
@@ -7,7 +8,7 @@ const config = {
 	kit: {
 		// hydrate the <div id="svelte"> element in src/app.html
 		target: '#svelte',
-		adapter: node()
+		adapter: netlify(),
 	}
 };
 
