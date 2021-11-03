ddb4887828d90ef1e46d018e20d598ac465175ec
moldeoreynosa <moldeorey@corning.com>
2021-11-03 13:33:13 -0500

    Issue De Fechas

--- src/hooks.js
+++ src/hooks.js
@@ -1,15 +1,15 @@
 export const NewGTE = () => {
-  let Day1 = new Date(Date.now() - 28800000).toLocaleDateString("en-US", { timeZone: "America/Chicago" })
+  let Day1 = new Date().toLocaleString("en-US", { timeZone: "America/Chicago" })
   return new Date(Day1 + " 08:00:00 AM")
 }
 
 export const NewLT = () => {
-  let Day2 = new Date(Date.now() + 57600000).toLocaleDateString("en-US", { timeZone: "America/Chicago" })
+  let Day2 = new Date().toLocaleString("en-US", { timeZone: "America/Chicago" })
   return new Date(Day2 + " 08:00:00 AM")
 }
 
 export const NewOEE = () => {
-  let Day3 = new Date(Date.now() - 28800000).toLocaleDateString("en-US", { timeZone: "America/Chicago" })
+  let Day3 = new Date().toLocaleString("en-US", { timeZone: "America/Chicago" })
   return new Date(Day3 + " 00:00:00 AM")
 }
 
