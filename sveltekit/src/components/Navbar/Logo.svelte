e8b5b1e9b18ce279765ef0ebf54259808cfb6f7e
moldeoreynosa <moldeorey@corning.com>
2021-11-16 08:56:16 -0600

    StrapiOnly

--- sveltekit/src/components/Navbar/Logo.svelte
+++
@@ -1,7 +0,0 @@
-<img src="/Logo.png" alt="" width="110" height="18" class="d-inline-block align-text-top">
-
-<style>
-  img {
-    margin-left: -5px;
-  }
-</style>
--- sveltekit/src/store.js
+++
@@ -1,6 +0,0 @@
-import { writable } from 'svelte/store'
-
-export const CurrentPath = writable("")
-export const Rates = writable([])
-export const ScrapCodes = writable([])
-export const FailureCodes = writable([])
