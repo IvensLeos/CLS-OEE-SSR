ffcff8ca7655b013de9215008a172e81a89ccd44
ivensleos <ivensleos8@gmail.com>
2021-12-17 01:38:01 -0600

    NextJS Code

--- pages/capture/data/[process].jsx
+++ pages/capture/data/[process].jsx
@@ -3,15 +3,15 @@ import NavpillsTab from "../../../components/Navpills/NavpillsTab"
 
 export const getServerSideProps = async ({ params }) => {
   const Process = params.process
-  const Oees = await fetch(process.env.SITE_URL + "api/capture/data/" + Process).then(r => r.json())
-  const FailureCodes = await fetch(process.env.SITE_URL + "api/failurecodes").then(r => r.json())
-  const ScrapCodes = await fetch(process.env.SITE_URL + "api/scrapcodes").then(r => r.json())
-  const Rates = await fetch(process.env.SITE_URL + "api/rates", {
+  const Oees = await fetch(process.env.VERCEL_URL + "api/capture/data/" + Process).then(r => r.json())
+  const FailureCodes = await fetch(process.env.VERCEL_URL + "api/failurecodes").then(r => r.json())
+  const ScrapCodes = await fetch(process.env.VERCEL_URL + "api/scrapcodes").then(r => r.json())
+  const Rates = await fetch(process.env.VERCEL_URL + "api/rates", {
     method: "POST",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify({ Process })
   }).then(r => r.json())
-  const Machines = await fetch(process.env.SITE_URL + "api/machines", {
+  const Machines = await fetch(process.env.VERCEL_URL + "api/machines", {
     method: "POST",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify({ Process })
