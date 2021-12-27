159941af4a5ede0958dd5f1ba9ee4bc4114e9c4c
Leos <LeosIO@corning.com>
2021-12-27 08:57:41 -0600

    Upgrade UI

--- components/Navbar/Logo.jsx
+++ components/Navbar/Logo.jsx
@@ -3,9 +3,9 @@ import Image from "next/image"
 const Logo = () => {
   return (
     <>
-      <Image src="/Logo.png" alt="Logo" width={110} height={18} className="Logo2" />
+      <Image src="/Logo.png" alt="Logo" width={110} height={18} className="Logo" />
       <style jsx>{`
-        :global(.Logo2) {
+        :global(.Logo) {
           margin-left: -2px !important;
         }
       `}</style>
