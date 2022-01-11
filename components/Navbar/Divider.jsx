655d8aba2e3773c6c9bc58efe8a6935203a3780e
Leos <LeosIO@corning.com>
2022-01-11 09:24:22 -0600

    MongoGUI

--- components/Navbar/Divider.jsx
+++
@@ -1,9 +0,0 @@
-const Divider = () => {
-  return (
-    <li>
-      <hr className="dropdown-divider" />
-    </li>
-  )
-}
-
-export default Divider
--- components/Navbar/TogglerButton.jsx
+++
@@ -1,9 +0,0 @@
-const TogglerButton = () => {
-  return (
-    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span className="navbar-toggler-icon"></span>
-    </button>
-  )
-}
-
-export default TogglerButton
