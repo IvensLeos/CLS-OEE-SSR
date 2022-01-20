a68981ea5039df1f47336fb3150bbcc93bedceb3
Leos <LeosIO@corning.com>
2022-01-19 18:46:31 -0600

    Svelted Data API

--- pages/api/index.js
+++ pages/api/index.js
@@ -17,3 +17,26 @@ export default async function handler(req, res) {
     PlantOEE: [...ParsedOEE(OEESBYPLANT)]
   })
 }
+
+// import { DatabaseConnection } from "../../util/mongodb"
+
+// export default async function handler(req, res) {
+//   const Connection = await DatabaseConnection()
+//   const AreasCollection = Connection.Database.collection("areas")
+//   const MachinesCollection = Connection.Database.collection("machines")
+//   const RatesCollection = Connection.Database.collection("rates")
+//   const ScrapCodesCollection = Connection.Database.collection("scrapcodes")
+//   const FailureCodesCollection = Connection.Database.collection("failurecodes")
+//   const OEESCollection = Connection.Database.collection("oees")
+
+//   let AREAS = await AreasCollection.find({}).project({ "_id": 0, "AREA": 1 }).toArray()
+//   let MACHINES = await MachinesCollection.find({ ACTIVE: true }).project({ "_id": 0, "MACHINE_NAME": 1, "PROCESS": 1 }).toArray()
+//   let RATES = await RatesCollection.find({}).project({ "_id": 0, "ITEM": 1, "MACHINE": 1, "RATE": 1, "WORK_CENTER": 1, "ROOT_AREA": 1 }).toArray()
+//   let SCRAPCODES = await ScrapCodesCollection.find({}).project({ "_id": 0, "SCRAPCODE": 1 }).toArray()
+//   let FAILURECODES = await FailureCodesCollection.find({}).project({ "_id": 0, "FAILURECODE": 1 }).toArray()
+//   let OEES = await OEESCollection.find({}).project({ "_id": 0 }).toArray()
+
+//   res.status(200).json({
+//     MACHINES
+//   })
+// }
--- pages/api/scrapcodes.js
+++ pages/api/scrapcodes.js
@@ -3,7 +3,7 @@ import { DatabaseConnection } from "../../util/mongodb"
 export default async function handler(req, res) {
   const Connection = await DatabaseConnection()
   const ScrapCodesCollection = Connection.Database.collection("scrapcodes")
-  const SCRAPCODES = await ScrapCodesCollection.find({}).map(Key => Key.SCRAPCODE).toArray()
+  const SCRAPCODES = await ScrapCodesCollection.find({}).project({ "_id": 0, "SCRAPCODE": 1 }).map(Key => Key.SCRAPCODE).toArray()
 
   res.status(200).json({
     SCRAPCODES
