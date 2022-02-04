6fa0a92f187e53dd065a863c8a48db7b2ecf0cb0
Leos <LeosIO@corning.com>
2022-02-04 14:13:56 -0600

    Merge From GenomicsBranch

--- util/hooks.js
+++ util/hooks.js
@@ -1,24 +1,18 @@
-// export const Day1 = new Date(Date.now() - 28800000).toLocaleDateString("en-US", { timeZone: "America/Chicago" })
-// export const Day2 = new Date(Date.now() + 57600000).toLocaleDateString("en-US", { timeZone: "America/Chicago" })
-export const Day1 = new Date(Date.now() - 115200000).toLocaleDateString("en-US", { timeZone: "America/Chicago" })
-export const Day2 = new Date(Date.now() - 28800000).toLocaleDateString("en-US", { timeZone: "America/Chicago" })
+export const Day1 = new Date(Date.now() - 28800000).toLocaleDateString("en-US", { timeZone: "America/Chicago" })
+export const Day2 = new Date(Date.now() + 57600000).toLocaleDateString("en-US", { timeZone: "America/Chicago" })
 
 export const NewGTE = () => {
-  let Day1 = new Date(Date.now() - 115200000).toLocaleDateString("en-US", { timeZone: "America/Chicago" })
-  // console.log({Day1})
+  let Day1 = new Date(Date.now() - 28800000).toLocaleDateString("en-US", { timeZone: "America/Chicago" })
   return new Date(Day1 + " 08:00:00 AM UTC")
 }
 
 export const NewLT = () => {
-  let Day2 = new Date(Date.now() - 28800000).toLocaleDateString("en-US", { timeZone: "America/Chicago" })
-  // console.log({Day2})
+  let Day2 = new Date(Date.now() + 57600000).toLocaleDateString("en-US", { timeZone: "America/Chicago" })
   return new Date(Day2 + " 08:00:00 AM UTC")
 }
 
-
 export const NewOEE = () => {
-  let Day3 = new Date(Date.now() - 115200000).toLocaleDateString("en-US", { timeZone: "America/Chicago" })
-  // console.log({Day3})
+  let Day3 = new Date(Date.now() - 28800000).toLocaleDateString("en-US", { timeZone: "America/Chicago" })
   return new Date(Day3 + " 00:00:00 AM UTC")
 }
 
